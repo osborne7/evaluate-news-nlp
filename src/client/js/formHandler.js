d6869bd5547ef5a51fb016fb3c8a0e869f4bfdce
osborne7 <eleanor.c.osborne@gmail.com>
2020-04-09 12:31:45 -0400

    Set up project with Webpack and Sass

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,10 +3,13 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
+
+    //use this as a reference to make references to an API for project
+    ///this didn't work, port wrong???
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
