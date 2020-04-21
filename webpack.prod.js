2e1c4babda310f822b888b8e4b665ae90f3eb90e
osborne7 <eleanor.c.osborne@gmail.com>
2020-04-21 19:26:55 -0400

    Require clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 
 module.exports = {
